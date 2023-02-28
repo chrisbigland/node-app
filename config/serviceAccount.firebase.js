ce19b43faa044864d8f42bf56c672048739cf586
CHRIS BIGLAND <christopherbigland@gmail.com>
2023-02-28 10:31:50 +0000

    node app created with basic find, findall, create and delete te and delete functionality. Find currently not working haven't tested delete yet

---
+++ config/serviceAccount.firebase.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "type": "service_account",
+  "project_id": "node-app-e34c1",
+  "private_key_id": "ea2e9806c7350beda955a822f7910deed43920d9",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC1OEKseDPDTNc7\n80ZLpJ3EqMNMhGiH6Cay3xLRxx1t5oTceqRWO9J1NpJ8eCJd1kCognCRy6p0AUyZ\n5Bt/njbymmbME25bi+V4BkRoQpm2npdC1zirxAB0HOoQYl2BB2bWRxh2b8VafVxj\nxydp28ff4BuGhkVRq7qCqB9TdsIfilutGfAgMEK2M1FxhL8XPFOD7DR+CvktS9NL\nnoKDKqAN9235NtbbE5SpeCV0TiNkcYFHqTUMSyQLIMJSzqCZHQLsnssGwlFmrbp4\nVe/wAzNArpya+r+BARY2FrP50RhN9Vs4SJ19WxpGVwAuDKYfECU4mTXEe3qMlDDW\nLugG9U7DAgMBAAECggEAAQwrCn8utsAgywvBdMTQqqcvwHyldApUqu+yK/yYKsn3\nK6UZLRcZxNavaxZwsGqBeNfkLwY73SwiwA2lq7ZY6qHCMAd5ZFW/L6iYSE1WQSF9\nOK7beNa1tUtzfkEV+Z3MK4yWqMdFn0Zz5xnAHGhfqTzM5plWbj3tYpL2jLog2M4m\nnZuC55Wh7/2nL/hhNN9hMTC6lzj1L8watMLVAZtKty2m+HkeZ45ARlri6ebx/QBX\nX+gvnC3HqJozDiniWU+md896KMsDP5UkmnJroYHART0nX1uvTxaGaBoNHdlrOgr4\n7n5KTgFJ9ynHw6sq8lELrD6kN9CkmfSfPtDF26SmWQKBgQDbP7pRxUfYsqc8PzG6\nnzMU+rz417MhcjnS4JDFwfkHYimrsRHSffcRJDikdptsiK9FXniaZXb0jFRLrj8Q\n98pGpcqmJd+rMe9qdVAJ9TiM5vyP1DxeYFq+FtBsJT7KJfSrV6Vh1LliFFjBpiNG\nDnjg8uRNNRMPbK+XChJImBT1aQKBgQDTmKYdVUBcTXtLLIj1boRp9l9485J4RsF4\nrn27jqXydF5FS1myQPZV1ceZr75Gt/IvIQL6lpKFgkX+VnBa5tB2lmFJEwAhIThG\nJwwnIH0WiHyW9gIRUPdTiAuuDL+HnyO7Kc67EKKj9HISwPJOuxzZTmRLBm91McMg\np0VGvXwBSwKBgAWky1AFiwbp+mITol0ScgctOSYSr1rCDPj/rNpaVuW4wifYRAQu\nF+kKDKc3ImMV1aUsfGBFEl3TKuxTzO3L4xoUdHX4bZyDiY0s/eVzuYPvAjtVlEQ9\nqLhR8AWIo+giN/0dIXW2OxtqOKqrcyb/gl8QAY53OklgW9jAep4BxDBZAoGBAJao\nnsZN+BiAlqQM3quZ+DAWWx4cl5KKFqFd5GpSBDhgwOqn3wK7nGm7YHY+GsXxnUAP\nSjawVT04Z6vTUgZ3R8DYDAbiv3nXVGEm1lFZoZcUsVkvaoN32dIhEOfpbcob9m4C\nM48gJh2FJF/T//Ms6TqUxXZh66RPPTcXwoy+x/eHAoGBAKr/YDg2n/v3iz8PRfPF\n3HJEO4crpd24+pXgL6WuX92slPXJN9E+5tGGkaLnbTo4OVseLd8kKC6pAmzsYTkm\nWJKTRYM+Tn4ZnlGaFODB0Fqhc1Csl5sPdk2Ri0jUPGSV1Q1gFmjQaZAUXesS9Vnw\nVVNIzm2jblevpMZFdZNGa7j9\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-qv6cz@node-app-e34c1.iam.gserviceaccount.com",
+  "client_id": "116163751374072238801",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-qv6cz%40node-app-e34c1.iam.gserviceaccount.com"
+}
